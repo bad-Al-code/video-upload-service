2628225b92ac08f9d65843c56e4ddb955921a03c
badAl <badalsingh120403@gmail.com>
2024-06-25 15:01:05 +0530

    number and bolean type

--- intro.ts
+++ intro.ts
@@ -3,4 +3,10 @@ let greetings: string = "Hi, Al";
 greetings.toLowerCase();
 console.log(greetings);
 
+// number
+// let userId: number = 3242342;
+let userId = 3242342;
+
+let isLoggedIn: boolean = false;
+
 export {};
