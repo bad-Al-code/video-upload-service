6ae12da9b14e5a253b931cb578aac8f94e54757c
Badal <badalsingh120403@gmail.com>
2025-02-16 19:13:38 +0530

    feat: send test email using resend

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-export function greet(name: string): string {
-    return `Hello, ${name}!`;
-}
+import 'dotenv/config';
 
-if (require.main === module) {
-    console.log(greet('World'));
-}
+import express, { Request, Response } from 'express';
+import { Resend } from 'resend';
+
+const app = express();
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+app.get('/', async (req: Request, res: Response): Promise<any> => {
+    const { data, error } = await resend.emails.send({
+        from: process.env.DOMAIN_NAME as string,
+        to: 'emailforsomework@gmail.com',
+        subject: 'hello world',
+        html: '<strong>it works!</strong>',
+    });
+
+    if (error) {
+        return res.status(400).json({ error });
+    }
+
+    res.status(200).json({ data });
+});
+
+app.listen(3000, () => {
+    console.log('Listening on http://localhost:3000');
+});
