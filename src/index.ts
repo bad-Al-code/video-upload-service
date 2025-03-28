5dce5f334186c3320a965f6250aa615d75b73e6b
Badal <badalsingh120403@gmail.com>
2025-03-28 16:26:23 +0000

    feat: unions and intersections types

--- src/index.ts
+++ src/index.ts
@@ -40,3 +40,35 @@ console.log(phones.work.area);
 // Tuples
 let myCar = [2020, 'Tesla', 'CarName'];
 const [year, make, model] = myCar;
+
+type OneThroughFive = 1 | 2 | 3 | 4 | 5;
+type Even = 2 | 4 | 6 | 8;
+
+let evenOrLowNumber = 5 as Even | OneThroughFive;
+
+function flipCoins() {
+  if (Math.random() > 0.5) return 'heads';
+  return 'tails';
+}
+
+const coinOutcome = flipCoins();
+
+const success = ['success', { name: 'Al', email: 'al@al.com' }] as const;
+const fail = ['error', new Error('Something went wrong')] as const;
+
+function maybeGetUserInfo() {
+  if (flipCoins() === 'heads') {
+    return success;
+  } else {
+    return fail;
+  }
+}
+
+const outcome2 = maybeGetUserInfo();
+const [first, second] = outcome2;
+
+if (second instanceof Error) {
+  second;
+} else {
+  second;
+}
