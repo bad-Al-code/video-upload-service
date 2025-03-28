8a54dfa461a15fadcea8251d993a27999def0f8c
Badal <badalsingh120403@gmail.com>
2025-03-28 16:05:15 +0000

    feat: object, arrays and tuple

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,42 @@
-function printName(name: string): string {
-  return `Hello ${name}`;
+interface Car {
+  make: string;
+  model: string;
+  year: number;
+  chargeVoltage?: number;
 }
-console.log(printName('Al'));
+
+function printCar(car: Car) {
+  let str = `${car.make} ${car.model} ${car.year}`;
+
+  if (typeof car.chargeVoltage !== 'undefined')
+    str += `// ${car.chargeVoltage}v`;
+
+  console.log(str);
+}
+
+printCar({
+  make: 'Tesla',
+  model: 'Model 4',
+  year: 2025,
+  chargeVoltage: 220,
+});
+
+interface Phone {
+  [k: string]: {
+    country: string;
+    area: string;
+    number: string;
+  };
+}
+
+const phones: Phone = {
+  home: { country: '+91', area: '110021', number: '912-123-1233' },
+  work: { country: '+91', area: '210021', number: '912-123-1233' },
+  fax: { country: '+91', area: '1230021', number: '912-123-1233' },
+};
+
+console.log(phones.work.area);
+
+// Tuples
+let myCar = [2020, 'Tesla', 'CarName'];
+const [year, make, model] = myCar;
