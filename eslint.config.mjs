6d0df66867a433d71a04818dd2036a0e6be700c3
Badal <badalsingh120403@gmail.com>
2025-03-28 14:28:33 +0000

    fix: prettier indentation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,10 +5,10 @@ import pluginSecurity from 'eslint-plugin-security';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { files: ['**/*.{js,mjs,cjs,ts}'] },
-    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginSecurity.configs.recommended,
-    { rules: { 'no-case-declarations': 'off' } },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
+  { rules: { 'no-case-declarations': 'off' } },
 ];
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+function printName(name: string): string {
+  return `Hello ${name}`;
+}
+console.log(printName('Al'));
