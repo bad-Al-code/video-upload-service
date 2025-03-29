87479629bfd56713fb7a018a660577d6e69dbd4c
Badal <badalsingh120403@gmail.com>
2025-03-29 16:02:57 +0000

    test: vitest test file setup

---
+++ tests/setup.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+
+import { vi, beforeEach, afterAll } from 'vitest';
+
+beforeEach(() => {});
+
+afterAll(() => {
+  console.log(`Finished running tests`);
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,23 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.ts'],
+      exclude: [
+        'src/db/**',
+        'src/config/**',
+        'src/index.ts',
+        'src/app.ts',
+        'src/errors/**',
+        'src/middleware/**',
+        'src/utils/**',
+        '**/*.test.ts',
+        '**/*.spec.ts',
+      ],
+    },
+    setupFiles: ['./tests/setup.ts'],
+    clearMocks: true,
   },
 });
