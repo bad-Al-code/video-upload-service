3bc190c42c2c954aea2dc215d412c1cc5432bed5
Badal <badalsingh120403@gmail.com>
2025-03-30 12:52:50 +0000

    feat(db): add 'UPLOADED' status to video schema
    - Updates the 'videoStatuses' enum in schema.ts to include 'UPLOADED'.
    - Regenerates Drizzle migration to apply the ENUM change to the database
    table."

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,21 +8,24 @@ import {
   timestamp,
 } from 'drizzle-orm/mysql-core';
 
-export const videoStatus = [
+export const videoStatuses = [
   'PENDING_UPLOAD',
   'UPLOAD_FAILED',
   'PROCESSING',
   'READY',
   'ERROR',
+  'UPLOADED',
 ] as const;
 
 export const videos = mysqlTable('videos', {
   id: varchar('id', { length: 36 }).primaryKey(),
   orignalFilename: varchar('original_filename', { length: 255 }).notNull(),
-  objectStorageKey: varchar('object_storage_key', { length: 255 }),
+  objectStorageKey: varchar('object_storage_key', { length: 1024 }),
   mimeType: varchar('mime_type', { length: 100 }).notNull(),
   sizeBytes: bigint('size_bytes', { mode: 'number', unsigned: true }).notNull(),
-  status: mysqlEnum('status', videoStatus).default('PENDING_UPLOAD').notNull(),
+  status: mysqlEnum('status', videoStatuses)
+    .default('PENDING_UPLOAD')
+    .notNull(),
   title: varchar('title', { length: 255 }),
   description: text('description'),
   durationSeconds: bigint('duration_seconds', {
@@ -36,4 +39,7 @@ export const videos = mysqlTable('videos', {
     .default(sql`CURRENT_TIMESTAMP`)
     .onUpdateNow()
     .notNull(),
+  uploadedAt: timestamp('uploaded_at'),
 });
+
+export type VideoStatus = (typeof videoStatuses)[number];
